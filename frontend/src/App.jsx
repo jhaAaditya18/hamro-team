27902040ffd5160f2c0fef2836742de9b227062b
imbivek08 <demoonx01@gmail.com>
2024-08-21 10:00:37 +0530

    frontend add

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>Welcome All</h1>
+        <h2>Lets Begin Coding</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
